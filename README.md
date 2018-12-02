e8748b9bcb03d7a3cf55e2c8db3886bf637ae825
Jason Kapp <mtbkapp@gmail.com>
2018-12-01 22:45:04 -0700

    Advent of Codegsgs

---
+++ README.md
@@ -0,0 +1,11 @@
+# advent-of-code-2018
+
+My 2018 Advent of Code solutions."
+
+## License
+
+Copyright © 2018 Jason Kapp
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
+
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject advent-of-code-2018 "0.1.0-SNAPSHOT"
+  :description "My 2018 Advent of Code solutions."
+  :url "https://github.com/mtbkapp/advent-of-code-2018"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.0-beta4"]])
