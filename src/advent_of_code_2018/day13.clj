03b19375918d8625dca6150ad923b41d6d6915df
Jason Kapp <mtbkapp@gmail.com>
2018-12-17 14:21:49 -0700

    mas

--- src/advent_of_code_2018/day13.clj
+++ src/advent_of_code_2018/day13.clj
@@ -6,7 +6,7 @@
             [loom.graph :as graph]))
 
 
-(println (first test-states))
+#_(println (first test-states))
 (def test-states
   (->> (slurp (io/resource "day13_test_states.txt"))
        (string/split-lines)
@@ -99,14 +99,21 @@
           g
           [(up p) (left p) (down p) (right p)]))
 
+(def cart->track
+  {\^ \|
+   \v \|
+   \< \-
+   \> \-})
+
 (defn add-cart
-  [state [x y] cart]
+  [state [x y :as p] cart]
   ; add edge to graph 
   ; update cart map
-  state
-  )
+  (-> state
+      (update :graph add-track p (cart->track cart))
+      (update :cart conj {:pos p :dir (cart-dirs cart)})))
 
-(parse-state (first test-states))
+#_(pprint (parse-state (first test-states)))
 (defn parse-state
   [s]
   (let [lines (string/split-lines s)
@@ -121,9 +128,82 @@
                   (= c \+) (update state :graph add-inter p cart-map)
                   (contains? cart-dirs c) (add-cart state p c)
                   :else state)))
-            {:carts {}
-             :graph (graph/graph)}
+            {:carts #{} :graph (graph/graph)}
             (for [y (range height)
                   x (range width)]
               [x y]))))
+
+(def test-nodes
+  #{[0 0] [1 0] [2 0] [3 0] [4 0]
+    [0 1]                   [4 1]             [7 1] [8 1] [9 1] [10 1] [11 1] [12 1]
+    [0 2]       [2 2] [3 2] [4 2] [5 2] [6 2] [7 2] [8 2] [9 2]               [12 2]
+    [0 3]       [2 3]       [4 3]             [7 3]       [9 3]               [12 3]
+    [0 4] [1 4] [2 4] [3 4] [4 4]             [7 4] [8 4] [9 4] [10 4] [11 4] [12 4]
+                [2 5] [3 5] [4 5] [5 5] [6 5] [7 5] [8 5] [9 5]})
+
+(def test-adj-list
+  {; right loop
+   [7 1] #{[8 1] [7 2]}
+   [8 1] #{[7 1] [9 1]}
+   [9 1] #{[8 1] [10 1]}
+   [10 1] #{[11 1] [9 1]}
+   [11 1] #{[10 1] [12 1]}
+   [12 1] #{[11 1] [12 2]}
+   [12 2] #{[12 1] [12 3]}
+   [12 3] #{[12 2] [12 4]}
+   [12 4] #{[12 3] [11 4]}
+   [11 4] #{[12 4] [10 4]}
+   [10 4] #{[11 4] [9 4]}
+   [9 4] #{[10 4] [9 3] [9 5] [8 4]}
+   [8 4] #{[9 4] [7 4]}
+   [7 4] #{[8 4] [7 3]}
+   [7 3] #{[7 4] [7 2]}
+   
+   ; center loop
+   [2 2] #{[3 2] [2 3]}
+   [3 2] #{[4 2] [2 2]}
+   [5 2] #{[4 2] [6 2]}
+   [6 2] #{[5 2] [7 2]}
+   [7 2] #{[7 1] [6 2] [8 2] [7 3]}
+   [8 2] #{[7 2] [9 2]}
+   [9 2] #{[8 2] [9 3]}
+   [9 3] #{[9 2] [9 5]}
+   [9 5] #{[9 4] [8 5]}
+   [8 5] #{[7 5] [9 5]}
+   [7 5] #{[8 5] [6 5]}
+   [6 5] #{[5 5] [4 5]}
+   [5 5] #{[4 5] [6 5]}
+   [4 5] #{[3 5] [5 5]}
+   [3 5] #{[4 5] [2 5]}
+   [2 5] #{[2 4] [3 5]}
+   [2 3] #{[2 4] [2 2]}
+
+   ; left loop
+   [0 0] #{[1 0] [0 1]}
+   [1 0] #{[0 0] [2 0]}
+   [2 0] #{[1 0] [3 0]}
+   [3 0] #{[2 0] [4 0]}
+   [4 0] #{[4 1] [3 0]}
+   [4 1] #{[4 0] [4 2]}
+   [4 2] #{[4 1] [4 3] [5 2] [3 2]}
+   [4 3] #{[4 2] [4 4]}
+   [4 4] #{[4 3] [3 4]}
+   [3 4] #{[2 4] [4 4]}
+   [2 4] #{[1 4] [3 4] [2 3] [2 5]}
+   [1 4] #{[2 4] [0 4]}
+   [0 4] #{[1 4] [0 3]}
+   [0 3] #{[0 2] [0 4]}
+   [0 2] #{[0 1] [0 3]}
+   [0 1] #{[0 0] [0 2]}})
+
+(deftest test-parse-state
+  (testing "nodes"
+    (let [{:keys [graph]} (parse-state (first test-states))]
+      (= test-nodes (into #{} (graph/nodes graph)))))
+  (testing "edges"
+    (doseq [n (graph/nodes)])
+
+    ))
+
+
 
