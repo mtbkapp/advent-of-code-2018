fb5d26fb1738cbece8ea6a1217cf31dc01fbddbf
Jason Kapp <jkapp@rallydev.com>
2018-12-17 15:20:26 -0700

    Start movement code

--- src/advent_of_code_2018/day13.clj
+++ src/advent_of_code_2018/day13.clj
@@ -14,7 +14,6 @@
        (partition 6)
        (map #(string/join \newline %))))
 
-
 (def first-turn :turn/left)
 
 (def next-turn
@@ -28,29 +27,22 @@
    \^ :dir/up
    \v :dir/down})
 
-(defn find-carts
-  [cart-map width height]
-  (for [y (range height)
-        x (range width)
-        :let [cart-dir (get cart-dirs (get-in cart-map [y x]))]
-        :when (some? cart-dir)]
-    {:pos [x y]
-     :dir cart-dir}))
+(defmulti move (fn [pos dir] dir))
 
-(defn up
-  [[x y]]
+(defmethod move :dir/up
+  [[x y] _]
   [x (dec y)])
 
-(defn down
-  [[x y]]
+(defmethod move :dir/down
+  [[x y] _]
   [x (inc y)])
 
-(defn left
-  [[x y]]
+(defmethod move :dir/left
+  [[x y] _]
   [(dec x) y])
 
-(defn right
-  [[x y]]
+(defmethod move :dir/right
+  [[x y] _]
   [(inc x) y])
 
 (def track-pieces #{\\ \/ \- \| \+ \> \< \^ \v})
@@ -59,9 +51,8 @@
   [g [x y :as p] section]
   (apply graph/add-edges g
          (case section
-           \- [[p (left p)] [p (right p)]]
-           \| [[p (up p)]
-               [p (down p)]])))
+           \- [[p (move p :dir/left)] [p (move p :dir/right)]]
+           \| [[p (move p :dir/up)] [p (move p :dir/down)]])))
 
 (defn get-in-cp
   [cp [x y]]
@@ -69,7 +60,10 @@
 
 (defn add-corner
   [g [x y :as p] corner cart-map]
-  (let [u (up p) l (left p) d (down p) r (right p)]
+  (let [u (move p :dir/up)
+        l (move p :dir/left) 
+        d (move p :dir/down)
+        r (move p :dir/right)]
     (cond
       ; -\  or |
       ;  |     \-
@@ -97,7 +91,7 @@
               (graph/add-edges g [p np])
               g))
           g
-          [(up p) (left p) (down p) (right p)]))
+          [(move p :dir/up) (move p :dir/left) (move p :dir/down) (move p :dir/down)]))
 
 (def cart->track
   {\^ \|
@@ -111,7 +105,9 @@
   ; update cart map
   (-> state
       (update :graph add-track p (cart->track cart))
-      (update :cart conj {:pos p :dir (cart-dirs cart)})))
+      (update :carts conj {:pos p
+                           :dir (cart-dirs cart)
+                           :last-turn :turn/right})))
 
 #_(pprint (parse-state (first test-states)))
 (defn parse-state
@@ -167,11 +163,11 @@
    [7 2] #{[7 1] [6 2] [8 2] [7 3]}
    [8 2] #{[7 2] [9 2]}
    [9 2] #{[8 2] [9 3]}
-   [9 3] #{[9 2] [9 5]}
+   [9 3] #{[9 2] [9 4]}
    [9 5] #{[9 4] [8 5]}
    [8 5] #{[7 5] [9 5]}
    [7 5] #{[8 5] [6 5]}
-   [6 5] #{[5 5] [4 5]}
+   [6 5] #{[5 5] [7 5]}
    [5 5] #{[4 5] [6 5]}
    [4 5] #{[3 5] [5 5]}
    [3 5] #{[4 5] [2 5]}
@@ -197,13 +193,79 @@
    [0 1] #{[0 0] [0 2]}})
 
 (deftest test-parse-state
-  (testing "nodes"
-    (let [{:keys [graph]} (parse-state (first test-states))]
-      (= test-nodes (into #{} (graph/nodes graph)))))
-  (testing "edges"
-    (doseq [n (graph/nodes)])
+  (let [{:keys [graph carts]} (parse-state (first test-states))
+        carts-by-pos (zipmap (map :pos carts)
+                             carts)]
+    (testing "nodes"
+      (= test-nodes (into #{} (graph/nodes graph))))
+    (testing "edges"
+      (doseq [n (graph/nodes graph)]
+        (is (= (test-adj-list n)
+               (set (graph/successors graph n))))))
+    (testing "carts"
+      (testing "cart at [2 0]"
+        (let [{:keys [pos dir last-turn] :as cart} (carts-by-pos [2 0])]
+          (is (some? cart))
+          (is (= [2 0] pos))
+          (is (= :dir/right dir))
+          (is (= :turn/right last-turn))))
+      (testing "cart at [9 3]"
+        (let [{:keys [pos dir last-turn] :as cart} (carts-by-pos [9 3])]
+          (is (some? cart))
+          (is (= [9 3] pos))
+          (is (= :dir/down dir))
+          (is (= :turn/right last-turn)))))))
 
-    ))
+(defn at-corner?
+  [{:keys [graph] :as state} {:keys [pos] :as cart}]
+  (let [[[x0 y0] [x1 y1] :as ss] (seq (graph/successors graph pos))]
+    (and (= 2 (count ss))
+         ; not co-linear
+         (not= x0 x1)
+         (not= y0 y1))))
 
+(defn at-intersection? 
+  [{:keys [graph] :as state} {:keys [pos] :as cart}]
+  (= 4 (count (graph/successors graph pos))))
 
+(defn move-cart-forward
+  [{:keys [graph] :as state} {:keys [pos dir] :as cart}]
+  (let [ss (graph/successors graph pos)
+        next-pos (move pos dir)]
+    (update state :carts
+            (fn [carts]
+              (-> carts
+                  (disj cart)
+                  (conj (assoc cart :pos next-pos)))))))
 
+(defn move-cart
+  [state {:keys [pos dir last-turn] :as cart}]
+  ; move along track
+  ; turn at a corner
+  ; at intersection, turn based on last-turn 
+  (cond (at-corner? state cart) state
+        (at-intersection? state cart) state
+        :else (move-cart-forward state cart)))
+
+(defn sort-carts
+  [carts]
+  (sort-by :pos
+           (fn [[x0 y0] [x1 y1]]
+             (if (= y0 y1)
+               (< x0 x1) 
+               (< y0 y1)))
+           carts))
+
+(defn next-state
+  [state]
+  (reduce move-cart state (sort-carts (:carts state))))
+
+(deftest test-next-state
+  (testing "check cart positions"
+    (let [s0 (parse-state (first test-states))
+          expected-carts (sort-carts (:carts (parse-state (second test-states))))
+          carts (sort-carts (:carts (next-state s0)))]
+      (is (= (-> expected-carts first :pos)
+             (-> carts first :pos)))
+      (is (= (-> expected-carts second :pos)
+             (-> carts second :pos))))))
